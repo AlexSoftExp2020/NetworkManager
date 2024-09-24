ed524ffcf6171143ea63a9330147fa991dd67da6
Alex <oleksii.pidhornyi@gmail.com>
2024-09-24 10:58:12 +0200

    Initial Commit

---
+++ NetworkManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkManager
+//
+//  Created by Oleksii on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
